61a1b9367e488bd1f66f3eddbe29ca9d8ae654b3
Hackbright Student <info@hackbrightacademy.com>
2015-11-09 14:24:31 -0800

    Settings page fields set up properly.

--- static/other.js
+++ static/other.js
@@ -1,14 +1,29 @@
 
-function showSettingsField(evt) {
-    var thisField = this.id;
-    var inputField = thisField.replace('edit-','');
-    inputField = document.getElementById(inputField);
-    console.log(inputField);
-    $('#email').removeAttr("hidden");
+function toggleSettingsField(evt) {
+    var editId = this.id;
+    var inputFieldId = editId.replace('edit-','#');
+
+    var fieldCurrentValueId = '#current-' + inputFieldId.replace('#','');
+    var fieldCurrentValueText = $(fieldCurrentValueId).text();
+
+    var fieldButtonId = '#button-' + inputFieldId.replace('#','');
+
+    console.log(inputFieldId, fieldCurrentValueId, fieldCurrentValueText);
+
+    if ($(inputFieldId).hasClass("temp-hidden")) {
+            $(inputFieldId).val(fieldCurrentValueText);
+            $(fieldButtonId).removeClass("temp-hidden");
+            $(inputFieldId).removeClass("temp-hidden");
+            $(fieldCurrentValueId).attr("hidden", "true");
+        } else {
+            $(inputFieldId).addClass("temp-hidden");
+            $(fieldButtonId).addClass("temp-hidden");
+            $(fieldCurrentValueId).removeAttr("hidden");
+        }
 }
 
 
 
 
 
-$('.click-to-edit').on("click", showSettingsField);
+$('.click-to-edit').on("click", toggleSettingsField);
